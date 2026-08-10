c964b3bc48e8b12a5616e8e12162f8f1ec339533
Jaskirat Singh <jas@ephpsolutions.com>
2026-08-10 15:02:21 +0530

    Stop Sending Emails to Tester

--- site/modules/site/frontend/warranty_log_daily_trade_notification_email.php
+++ site/modules/site/frontend/warranty_log_daily_trade_notification_email.php
@@ -131,7 +131,7 @@ foreach ($suppliers as $supplier => $data) {
 	}
 	//$emailObj->addTo($customerEmail, $customerName);
 	$emailObj->addTo('rahul@ephpsolutions.com', 'Rahul');
-	$emailObj->addTo('arun@ephpsolutions.com', 'Tester');
+	//$emailObj->addTo('arun@ephpsolutions.com', 'Tester');
 	$emailObj->attachments = [];
 	
 	$response = $emailObj->sendEmail();
